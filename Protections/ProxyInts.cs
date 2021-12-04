3f94fa1cb6b52796d593715c2078acfb56b22f9e
Snow <snowasdev@gmail.com>
2021-12-05 00:39:47 +0100

    better int proxy

--- Protections/ProxyInts.cs
+++ Protections/ProxyInts.cs
@@ -60,6 +60,46 @@ namespace kov.NET
                     }
                 }
             }
+
+            var stringref = new TypeRefUser(ManifestModule, "System", "String", ManifestModule.CorLibTypes.AssemblyRef);
+            var stringlength = new MemberRefUser(ManifestModule, "get_Length", MethodSig.CreateInstance(ManifestModule.CorLibTypes.Int32), stringref);
+            var mathref = new TypeRefUser(ManifestModule, "System", "Math", ManifestModule.CorLibTypes.AssemblyRef);
+            var mathmin = new MemberRefUser(ManifestModule, "Min", MethodSig.CreateStatic(ManifestModule.CorLibTypes.Int32, ManifestModule.CorLibTypes.Int32, ManifestModule.CorLibTypes.Int32), mathref);
+            var systemconvert = new TypeRefUser(ManifestModule, "System", "Convert", ManifestModule.CorLibTypes.AssemblyRef);
+            var toint32 = new MemberRefUser(ManifestModule, "ToInt32", MethodSig.CreateStatic(ManifestModule.CorLibTypes.Int32, ManifestModule.CorLibTypes.String), systemconvert);
+
+            foreach (var type in ManifestModule.GetTypes())
+            {
+                foreach (var method in type.Methods)
+                {
+                    if (!method.HasBody || method.Body == null) continue;
+
+                    for (int i = 0; i < method.Body.Instructions.Count(); i++)
+                    {
+                        var instr = method.Body.Instructions;
+                        if (instr[i].IsLdcI4() && instr[i].GetLdcI4Value() >= 0 && instr[i].GetLdcI4Value() <= 1000)
+                        {
+                            int amount = instr[i].GetLdcI4Value();
+                            instr[i].OpCode = OpCodes.Ldstr;
+                            instr[i].Operand = L2F.RandomString(amount);
+                            instr.Insert(i + 1, Instruction.Create(OpCodes.Call, stringlength));
+                            instr.Insert(i + 2, Instruction.Create(OpCodes.Ldstr, L2F.RandomString(amount + 1)));
+                            instr.Insert(i + 3, Instruction.Create(OpCodes.Call, stringlength));
+                            instr.Insert(i + 4, Instruction.Create(OpCodes.Call, mathmin));
+                            i += 4;
+                        }else if (instr[i].IsLdcI4())
+                        {
+                            int amount = instr[i].GetLdcI4Value();
+                            instr[i].OpCode = OpCodes.Ldstr;
+                            instr[i].Operand = amount.ToString();
+                            instr.Insert(i + 1, Instruction.Create(OpCodes.Call, toint32));
+                        }
+                    }
+                }
+
+            }
+
+
             Console.WriteLine("   " + Amount + " ints proxied!");
         }
     }
